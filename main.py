c5263593cc1065cb40f62fd0db24d984da8aca38
Pedro Pires <ph_pires@poli.ufrj.br>
2025-07-21 12:02:07 -0300

    Getting tags info

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import subprocess
+import json
+from classes.godot_remote_tags import GodotRemoteTags
+
 def main():
     result = subprocess.run(["curl", "https://api.github.com/repos/godotengine/godot/tags"], capture_output=True, text=True)
-    print(result.stdout)
+    parsed_result = json.loads(result.stdout)
+    godot_remote_tags = []
+    for r in parsed_result:
+        godot_remote_tags.append(GodotRemoteTags(r["name"]))
     
 if __name__ == "__main__":
     main()
